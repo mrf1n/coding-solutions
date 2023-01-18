26d966b527eef51d66a555d66803184338baff66
Mr F1N <mrf1n@github.com>
2023-01-18 19:01:15 +0100

    T26, T66, T122, T217, T350 have been solved

---
+++ src/main/java/by/mrf1n/T0066_PlusOne.java
@@ -0,0 +1,35 @@
+package by.mrf1n;
+
+import java.util.Arrays;
+
+public class T0066_PlusOne {
+    public static void main(String[] args) {
+        int[] digits = new int[]{4, 3, 2, 1, 9};
+        System.out.println(Arrays.toString(new T0066_PlusOne().plusOne(digits)));
+    }
+
+    public int[] plusOne(int[] digits) {
+        int plusNumber = 1;
+        int i = digits.length - 1;
+        while (plusNumber > 0 && i >= 0) {
+            int digitsSum = digits[i] + plusNumber;
+            if (digitsSum > 9) {
+                digits[i] = digitsSum % 10;
+                plusNumber = digitsSum / 10;
+            } else {
+                digits[i] = digitsSum;
+                plusNumber = 0;
+            }
+            i--;
+        }
+        if (plusNumber > 0) {
+            int[] oldDigits = digits;
+            digits = new int[oldDigits.length + 1];
+            digits[0] = plusNumber;
+            for (i = 1; i < digits.length; i++) {
+                digits[i] = oldDigits[i - 1];
+            }
+        }
+        return digits;
+    }
+}
