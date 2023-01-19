97965626c4d803778e4170aec6b998cc8aba6edf
Mr F1N <mrf1n@github.com>
2023-01-19 19:04:14 +0100

    T8, T14, T28, T36, T48, T125, T242, T387 have been solved

--- src/main/java/by/mrf1n/T0876_MiddleOfLinkedList.java
+++ src/main/java/by/mrf1n/T0876_MiddleOfLinkedList.java
@@ -5,15 +5,15 @@ import by.mrf1n.common.ListNode;
 public class T0876_MiddleOfLinkedList {
     public static void main(String[] args) {
         ListNode test01 = new ListNode(1);
-        ListNode test02 = new ListNode(1,new ListNode(2));
-        ListNode test03 = new ListNode(1,new ListNode(2,new ListNode(3)));
-        ListNode test04 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
-        ListNode test05 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
-        ListNode test06 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6))))));
-        ListNode test07 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6, new ListNode(7)))))));
-        ListNode test08 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8))))))));
-        ListNode test09 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9)))))))));
-        ListNode test10 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9, new ListNode(10))))))))));
+        ListNode test02 = new ListNode(1, new ListNode(2));
+        ListNode test03 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode test04 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode test05 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode test06 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        ListNode test07 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7)))))));
+        ListNode test08 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8))))))));
+        ListNode test09 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9)))))))));
+        ListNode test10 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9, new ListNode(10))))))))));
         System.out.println(new T0876_MiddleOfLinkedList().middleNode(test01));
         System.out.println(new T0876_MiddleOfLinkedList().middleNode(test02));
         System.out.println(new T0876_MiddleOfLinkedList().middleNode(test03));
@@ -31,7 +31,7 @@ public class T0876_MiddleOfLinkedList {
         int i = 1;
         while (tail != null) {
             tail = tail.next;
-            if (i <2) {
+            if (i < 2) {
                 i++;
             } else {
                 if (middle.next != null) {
