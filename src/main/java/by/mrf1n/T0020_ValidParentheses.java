0063c05f261c252777e22b7d85652c734f192f15
Mr F1N <mrf1n@github.com>
2023-01-23 13:58:28 +0100

    T13, T20, T118, T268, T326, T461 have been solved

---
+++ src/main/java/by/mrf1n/T0020_ValidParentheses.java
@@ -0,0 +1,90 @@
+package by.mrf1n;
+
+import java.util.Stack;
+
+public class T0020_ValidParentheses {
+
+    public static void main(String[] args) {
+        String[] strings0 = new String[]{"", "(", "(()", "(){}", "(}"};
+        for (String string : strings0) {
+            System.out.println(new T0020_ValidParentheses().isValid(string));
+        }
+        System.out.println();
+        String[] strings1 = new String[]{"", "(", "(()", "(){}", "(}"};
+        for (String string : strings1) {
+            System.out.println(new T0020_ValidParentheses().isValidSwitch(string));
+        }
+        System.out.println();
+        String[] strings2 = new String[]{"", "(", "(()", "(){}", "(}"};
+        for (String string : strings2) {
+            System.out.println(new T0020_ValidParentheses().isValidElseIf(string));
+        }
+        System.out.println();
+    }
+
+    public boolean isValid(String s) {
+        if (s.length() == 0 || s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char bracket : s.toCharArray()) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+            } else if (!stack.empty() && Math.abs(bracket - stack.peek()) < 3) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+
+    public boolean isValidSwitch(String s) {
+        if (s.length() == 0 || s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char bracket : s.toCharArray()) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                boolean isEndBracket = switch (bracket) {
+                    case ')' -> stack.peek() == '(';
+                    case ']' -> stack.peek() == '[';
+                    case '}' -> stack.peek() == '{';
+                    default -> false;
+                };
+
+                if (isEndBracket) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+
+    public boolean isValidElseIf(String s) {
+        if (s.length() == 0 || s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char bracket : s.toCharArray()) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+            } else if (stack.empty()) {
+                return false;
+            } else if (bracket == ')' && stack.peek() == '('
+                    || bracket == ']' && stack.peek() == '['
+                    || bracket == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
---
+++ src/main/java/by/mrf1n/T0268_MissingNumber.java
@@ -0,0 +1,23 @@
+package by.mrf1n;
+
+public class T0268_MissingNumber {
+
+    public static void main(String[] args) {
+        int[] nums0 = new int[]{3, 0, 1};
+        System.out.println(new T0268_MissingNumber().missingNumber(nums0));
+        int[] nums1 = new int[]{0, 1};
+        System.out.println(new T0268_MissingNumber().missingNumber(nums1));
+        int[] nums2 = new int[]{9,6,4,2,3,5,7,0,1};
+        System.out.println(new T0268_MissingNumber().missingNumber(nums2));
+    }
+
+    public int missingNumber(int[] nums) {
+        int out = 0;
+        out ^= nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            out ^= nums[i];
+            out ^= i;
+        }
+        return out;
+    }
+}
