cada3caf6bf2684b70c8179d94cde520c1fab79f
Mr F1N <mrf1n@github.com>
2023-06-09 10:12:34 +0200

    T383, T1342, T1480, T1672 have been solved. T412 have been updated with LC solution.

--- src/main/java/by/mrf1n/T0412_FizzBuzz.java
+++ src/main/java/by/mrf1n/T0412_FizzBuzz.java
@@ -8,7 +8,8 @@ public class T0412_FizzBuzz {
     public static void main(String[] args) {
         System.out.println(new T0412_FizzBuzz().fizzBuzz(5));
         System.out.println(new T0412_FizzBuzz().fizzBuzz(15));
-        System.out.println(new T0412_FizzBuzz().fizzBuzz(1));
+        System.out.println(new T0412_FizzBuzz().fizzBuzzLC(15));
+        System.out.println(new T0412_FizzBuzz().fizzBuzzLC(1));
     }
 
     public List<String> fizzBuzz(int n) {
@@ -23,6 +24,24 @@ public class T0412_FizzBuzz {
             } else {
                 strings.add(String.valueOf(i));
             }
+        }
+        return strings;
+    }
+
+    public List<String> fizzBuzzLC(int n) {
+        List<String> strings = new ArrayList<>();
+        for (int i = 1; i < n + 1; i++) {
+            String currStr = "";
+            if (i % 3 == 0) {
+                currStr += "Fizz";
+            }
+            if (i % 5 == 0) {
+                currStr += "Buzz";
+            }
+            if (currStr.isEmpty()) {
+                currStr += String.valueOf(i);
+            }
+            strings.add(currStr);
         }
         return strings;
     }
