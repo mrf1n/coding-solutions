7c463b25dc635579eb33ce6fe343e8d94fe5b0cc
mrf1n <mrf1n@github.com>
2024-04-08 00:57:31 +0200

    Catcoder T0557_L01_D2_Fibonacci and T1207_L01_D1_DeletablePrimes

---
+++ catcoder-codingcontest-org/src/main/kotlin/com/github/mrf1n/threeninethclassic/training/T0557_L01_D2_Fibonacci.kt
@@ -0,0 +1,31 @@
+package com.github.mrf1n.threeninethclassic.training
+
+fun main(args: Array<String>) {
+    val numbers = listOf(
+        6,
+        19,
+        28,
+        36,
+        38,
+    )
+    numbers.forEach {
+        println("$it ${fib(it)}")
+    }
+}
+
+var cache: MutableMap<Int, Int> = HashMap()
+
+fun fib(n: Int): Int {
+    if (cache.containsKey(n)) {
+        return cache[n]!!
+    }
+
+    val result = if (n < 2) {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+
+    cache[n] = result
+    return result
+}
---
+++ catcoder-codingcontest-org/src/main/kotlin/com/github/mrf1n/threeninethclassic/training/T1207_L01_D1_DeletablePrimes.kt
@@ -0,0 +1,48 @@
+package com.github.mrf1n.threeninethclassic.training
+
+import java.util.stream.IntStream
+import kotlin.math.sqrt
+
+fun main(args: Array<String>) {
+    val numbers = listOf(
+        "4125673",
+        "41256793",
+        "337424981",
+        "537430451",
+        "200899998",
+        "537499093",
+        "2147483059",
+        "410256793",
+        "567629137",
+        "46216567629137",
+    )
+    numbers.forEach {
+        println(it + " " + countSubPrimes(it))
+    }
+}
+
+fun countSubPrimes(number: String): Int {
+    return if (number.isNotEmpty() && number.isPrime()) {
+        when {
+            number.length == 1 -> 1
+            number.length > 1 -> {
+                var count = 0
+                for (i in number.indices) {
+                    val subNumber = number.removeRange(IntRange(i, i))
+                    count += countSubPrimes(subNumber)
+                }
+                count
+            }
+
+            else -> 0
+        }
+    } else 0
+}
+
+fun String.isPrime(): Boolean = isPrime(this.toLong())
+
+fun isPrime(number: Long): Boolean {
+    return if (number > 1) IntStream.rangeClosed(2, sqrt(number.toDouble()).toInt())
+        .noneMatch { n: Int -> (number % n == 0L) } else false
+}
+
